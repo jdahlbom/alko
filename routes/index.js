e6804b282e070c92aba068f826c5c5ab24f8b403
Teemu Tiilikainen <teemu.tiilikainen@conmio.com>
2014-04-09 16:59:15 +0300

    sellasta

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,6 @@
+exports.index = function(req, res) {
 
-/*
- * GET home page.
- */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+
+  	res.render('index', { title: 'Express' });
 };
