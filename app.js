d7c17953e7aae56b0923af50a333d71e62246cc6
VarmaIS <user@server>
2014-04-26 11:22:05 +0300

    Lisätty connect ja pakkaus

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-var utils = require('./utils.js');
+var utils 	= require('./utils.js');
 var express = require('express');
-var http = require('http');
-var path = require('path');
+var http 	= require('http');
+var path 	= require('path');
+var connect = require('connect');
 
 var app = express();
 
@@ -10,11 +11,11 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.favicon());
+app.use(connect.compress());
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
-app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
